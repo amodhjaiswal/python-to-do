b07a6a166a8ecb679ea05c7680c973cf7ec3dc8e
Amodh Jaiswal <60597059+amodhjaiswal@users.noreply.github.com>
2024-11-04 13:23:50 +0530

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/demo-python-app/python/app
+nohup python3 app.py &
+# Activate virtual environment if needed
+# source venv/bin/activate  # Uncomment if you're using a virtual environment
